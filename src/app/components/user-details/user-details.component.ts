242569ee5592ec36232a834dfaddb914f1c244f7
DiegoArruda <arrudadiego1@gmail.com>
2025-05-26 17:57:17 -0300

    user details receiving User type

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { User } from '../../user';
 @Component({
   selector: 'app-user-details',
   imports: [],
@@ -6,7 +7,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrl: './user-details.component.css',
 })
 export class UserDetailsComponent {
-  @Input() user: any;
+  @Input() user!: User;
   @Input() isOpen: boolean = false;
   @Output() close = new EventEmitter<void>();
 
